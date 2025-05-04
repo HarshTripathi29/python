ac2249c795b00c92c4033ac094791d88a4b1e579
Harsh Tripathi <harshtripathi042@gmail.com>
2025-05-04 16:46:48 +0530

    functions

---
+++ function/basic-function.py
@@ -0,0 +1,14 @@
+def multiply(x,y):
+    return x*y
+
+a=6
+b=8
+
+product = multiply(a,b)
+print(f"the product of {a} and {b} : {product}")
+
+def greet_user(name, greeting="Hello"):
+    print(f"{greeting}, {name}")
+
+greet_user("Harsh")
+greet_user("Rajat", "Hi")
